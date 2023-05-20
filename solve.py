8aba6d8976fbe50a46e97248a74dae8f80185a43
Emirkan Eşme <108766935+rootebby@users.noreply.github.com>
2023-05-19 20:00:48 -0400

    Add files via upload

---
+++ solve.py
@@ -0,0 +1,42 @@
+from os import system,name
+import numpy as np
+from time import sleep
+
+
+if name == 'nt':
+    system("cls")
+else:
+    system("clear")
+
+
+print("""
+                 _    ____       _     _           
+ _ __ ___   ___ | |_ / __ \  ___| |__ | |__  _   _ 
+| '__/ _ \ / _ \| __/ / _` |/ _ \ '_ \| '_ \| | | |
+| | | (_) | (_) | || | (_| |  __/ |_) | |_) | |_| |
+|_|  \___/ \___/ \__\ \__,_|\___|_.__/|_.__/ \__, |
+                     \____/                  |___/ 
+                                                   
+""")
+sleep(2)
+i=1
+print("Dont forget to make your file !!!!")
+print("Creating array with zeros")
+arr = np.empty(32, dtype=object)
+sleep(2)
+filename = input("Enter your file name with extension (press enter for data.txt) : ")
+if len(filename) == 0:
+    filename = 'data.txt'
+with open(filename,"r",encoding="UTF-8") as data:
+    for line in data:
+        start = line.index('(')
+        stop = line.index(')')
+        num = int(line[start+1:stop])
+        dat = line[-6:-5:]
+        print("On line {} , index determined as {} , letter determined as {}".format(i,num,dat))
+        i+=1
+        arr[num] = str(dat)
+        sleep(0.05)
+
+my_data = ''.join(i for i in arr)
+print("FLAG DECODED !!!! :\t",my_data)
