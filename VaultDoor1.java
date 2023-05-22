44900fe718f1ba8b07b44b41a89a1eb2f2d2be9e
Emirkan Eşme <108766935+rootebby@users.noreply.github.com>
2023-05-22 08:55:34 -0400

    Update VaultDoor1.java

--- VaultDoor1.java
+++ VaultDoor1.java
@@ -1,57 +1,38 @@
-import java.util.*;
+#include <stdio.h>
 
-class VaultDoor1 {
-    public static void main(String args[]) {
-        VaultDoor1 vaultDoor = new VaultDoor1();
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter vault password: ");
-	String userInput = scanner.next();
-	String input = userInput.substring("picoCTF{".length(),userInput.length()-1);
-	if (vaultDoor.checkPassword(input)) {
-	    System.out.println("Access granted.");
-	} else {
-	    System.out.println("Access denied!");
-	}
-    }
+void reverseArray(int* arr, int size) {
+    int* start = arr;
+    int* end = arr + size - 1;
 
-    // I came up with a more secure way to check the password without putting
-    // the password itself in the source code. I think this is going to be
-    // UNHACKABLE!! I hope Dr. Evil agrees...
-    //
-    // -Minion #8728
-    public boolean checkPassword(String password) {
-        return password.length() == 32 &&
-               password.charAt(0)  == 'd' &&
-               password.charAt(29) == '9' &&
-               password.charAt(4)  == 'r' &&
-               password.charAt(2)  == '5' &&
-               password.charAt(23) == 'r' &&
-               password.charAt(3)  == 'c' &&
-               password.charAt(17) == '4' &&
-               password.charAt(1)  == '3' &&
-               password.charAt(7)  == 'b' &&
-               password.charAt(10) == '_' &&
-               password.charAt(5)  == '4' &&
-               password.charAt(9)  == '3' &&
-               password.charAt(11) == 't' &&
-               password.charAt(15) == 'c' &&
-               password.charAt(8)  == 'l' &&
-               password.charAt(12) == 'H' &&
-               password.charAt(20) == 'c' &&
-               password.charAt(14) == '_' &&
-               password.charAt(6)  == 'm' &&
-               password.charAt(24) == '5' &&
-               password.charAt(18) == 'r' &&
-               password.charAt(13) == '3' &&
-               password.charAt(19) == '4' &&
-               password.charAt(21) == 'T' &&
-               password.charAt(16) == 'H' &&
-               password.charAt(27) == '5' &&
-               password.charAt(30) == '2' &&
-               password.charAt(25) == '_' &&
-               password.charAt(22) == '3' &&
-               password.charAt(28) == '0' &&
-               password.charAt(26) == '7' &&
-               password.charAt(31) == 'e';
+    while (start < end) {
+        // Swap the elements pointed by start and end
+        int temp = *start;
+        *start = *end;
+        *end = temp;
+
+        // Move the pointers inward
+        start++;
+        end--;
     }
+}
+
+int main() {
+    int arr[] = {66, 94, 26, 69, 46, 76, 1, 93, 73, 62, 77, 9};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    reverseArray(arr, size);
+
+    printf("Reversed array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
